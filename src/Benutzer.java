4fa1cd20c6fd9c1a11ec02e67b2ff5b3f38c8cc3
RubWolff <114943930+RubWolff@users.noreply.github.com>
2023-03-08 14:57:21 +0100

    benutzer

---
+++ src/Benutzer.java
@@ -0,0 +1,17 @@
+public class Benutzer{
+    String name;
+    int kundenNummer;
+
+    public Benutzer(String name, int kundenNummer){
+        this.name = name;
+        this.kundenNummer = kundenNummer;
+    }
+
+    public String getName(){
+        return name;
+    }
+    public int getKundenNummer(){
+        return name;
+    }
+
+}

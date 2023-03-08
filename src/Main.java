50fa52ac67b4a93b787bb09d85360adc168333c4
RubWolff <114943930+RubWolff@users.noreply.github.com>
2023-03-08 15:05:50 +0100

    main

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+import java.util.Arraylist;
+
+
+public class Main{
+    ArrayList<Benutzer> benutzer = new ArrayList<Benutzer>();
+    
+    pulbic static void main(String[] args){
+        App gui = new App();
+    }
+
+}

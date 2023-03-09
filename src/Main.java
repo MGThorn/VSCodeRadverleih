88fcd0efa8e599977b525f9eb0e6965808ca18da
MGThorn <107687299+MGThorn@users.noreply.github.com>
2023-03-09 13:38:10 +0100

    main try catch

--- src/Main.java
+++ src/Main.java
@@ -5,7 +5,12 @@ public class Main{
     ArrayList<Benutzer> benutzer = new ArrayList<Benutzer>();
     
     public static void main(String[] args){
-        GUI gui = new GUI();
+        try {
+            GUI gui = new GUI();
+        } catch (Exception e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
     }
 
 }
